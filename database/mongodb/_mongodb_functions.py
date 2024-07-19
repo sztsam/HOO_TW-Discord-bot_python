394f7a3819aa684a98891203451d9c1834b35a14
simisami <sztsam3@gmail.com>
2024-07-19 20:53:20 +0200

    Update _mongodb_functions.py
    added missing update_guild_config

--- database/mongodb/_mongodb_functions.py
+++ database/mongodb/_mongodb_functions.py
@@ -1,5 +1,6 @@
 import importlib
 import os
+from database.mongodb.update_guild_config._update_guild_config import update_guild_config
 
 directory = os.path.dirname(os.path.abspath(__file__))
 module_path = "database.mongodb."
@@ -10,6 +11,7 @@ functions = [name[:-3] for name in os.listdir(directory)
 
 
 def mongodb_functions(self):
+    setattr(self, "update_guild_config", update_guild_config)
     for function in functions:
         function_path = f"{module_path}{function}"
         imported_module = importlib.import_module(function_path)
